81f873b835ac80f1b992d2161a290672a47fc717
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-04-28 11:48:04 -0400

    Update teamRoutes.js

--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const ctrl = require("../controllers/teamController");
+const { requireAuth } = require("../middleware/authMiddleware");
 
+router.post("/", requireAuth, ctrl.createTeam);
+router.get("/", requireAuth, ctrl.getTeams);
+router.post("/:teamId/pokemon", requireAuth, ctrl.addPokemon);
+router.delete("/:id", requireAuth, ctrl.deleteTeam);
+
+module.exports = router;
