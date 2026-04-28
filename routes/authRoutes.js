a15d100c0fb8fc3aad0caf856b05ff0799e6306d
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-04-28 13:03:48 -0400

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,7 @@ const router = express.Router();
 
 const authController = require("../controllers/authController");
 
-// REGISTER
 router.post("/register", authController.register);
-
-// LOGIN
 router.post("/login", authController.login);
 
 module.exports = router;
