cecfc30f423f40de0313a7fdd67c2db4406d56b5
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-04-28 11:52:43 -0400

    Log current timestamp query results to console

--- db.js
+++ db.js
@@ -10,4 +10,8 @@ const pool = new Pool({
 
 module.exports = {
     query: (text, params) => pool.query(text, params)
+
+db.query("SELECT NOW()")
+  .then(res => console.log(res.rows))
+  .catch(err => console.error(err));
 };
