4a8ceba98f33a4e991296d7f97b7b33fbca3266f
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-04-28 12:11:20 -0400

    Update database password in db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "yourpassword",
+    password: "GhoulURG1488::))",
     database: "pokemon_api",
     waitForConnections: true,
     connectionLimit: 10
