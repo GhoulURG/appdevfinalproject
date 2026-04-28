3dfbf0de3e7e23c2cb674e11cab1bdfa3cdbb23d
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-04-28 11:47:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const session = require("express-session");
 
+const app = express();
+
+app.use(express.json());
+
+app.use(session({
+    secret: "supersecretkey",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/teams", require("./routes/teamRoutes"));
+
+app.use(require("./middleware/errorMiddleware"));
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
