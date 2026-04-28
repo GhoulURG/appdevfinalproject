a8ff655fcb5dfe1740c92e29c4ad5c6f23493113
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-04-28 12:48:16 -0400

    Change session secret and add root API route
    Updated session secret and added a root route.

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ const app = express();
 app.use(express.json());
 
 app.use(session({
-    secret: "supersecretkey",
+    secret: "pokemon_secret",
     resave: false,
     saveUninitialized: false
 }));
 
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
+
 app.use("/auth", require("./routes/authRoutes"));
 app.use("/teams", require("./routes/teamRoutes"));
-
-app.use(require("./middleware/errorMiddleware"));
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
