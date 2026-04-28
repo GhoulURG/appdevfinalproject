3cab11dc04b3cc4ac06dd458b0295d79ea75ed14
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-04-28 13:04:15 -0400

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,6 @@
-function requireAuth(req, res, next) {
-    if (!req.session.userId) {
+module.exports = (req, res, next) => {
+    if (!req.session.user) {
         return res.status(401).json({ error: "Unauthorized" });
     }
     next();
-}
-
-module.exports = { requireAuth };
+};
