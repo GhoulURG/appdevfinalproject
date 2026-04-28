17824e464fd55e41a2adb7de0ae0d3fc6fd16493
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-04-28 12:17:29 -0400

    Update database connection configuration
    Removed database password and adjusted connection settings.

--- db.js
+++ db.js
@@ -3,10 +3,9 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "GhoulURG1488::))",
+    password: "",
     database: "pokemon_api",
-    waitForConnections: true,
-    connectionLimit: 10
+    port: 3306
 });
 
 module.exports = pool;
