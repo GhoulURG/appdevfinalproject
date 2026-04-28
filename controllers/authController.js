8785845105078744f9e647b55570c436aebb4fd5
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-04-28 11:47:34 -0400

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,39 @@
+const bcrypt = require("bcrypt");
+const db = require("../db");
 
+exports.register = async (req, res) => {
+    const { username, password } = req.body;
+
+    const hash = await bcrypt.hash(password, 10);
+
+    await db.query(
+        "INSERT INTO users (username, password_hash) VALUES ($1, $2)",
+        [username, hash]
+    );
+
+    res.json({ message: "User created" });
+};
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+
+    const result = await db.query(
+        "SELECT * FROM users WHERE username=$1",
+        [username]
+    );
+
+    const user = result.rows[0];
+
+    if (!user || !(await bcrypt.compare(password, user.password_hash))) {
+        return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    req.session.userId = user.id;
+
+    res.json({ message: "Logged in" });
+};
+
+exports.logout = (req, res) => {
+    req.session.destroy();
+    res.json({ message: "Logged out" });
+};
